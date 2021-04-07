6b45a4af890e32c910e87900f46c782e87699889
kmxionghang <1364052798@qq.com>
2021-04-07 15:55:48 +0800

    话题列表

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
