0300c327e89ca1a11352432fef3c3ed27398fbab
kmxionghang <1364052798@qq.com>
2021-04-07 17:33:25 +0800

    图片上传

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
